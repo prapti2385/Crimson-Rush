670253119b9f915655b8a71bf6e85a263088a63f
prapti2385 <praptikhandelwal14@gmail.com>
2025-07-31 20:23:01 +0530

    health management to player and animations rendering

--- mob.gd
+++ mob.gd
@@ -1,8 +1,24 @@
 extends CharacterBody2D
 @onready var player: CharacterBody2D = $"../Player"
+@onready var slime: Node2D = %Slime
+var health = 3
 
+
+func _ready() -> void:
+	slime.play_walk()
+	
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position);
 	velocity = direction * 300.0
 	move_and_slide()
+	
+func take_damage():
+	health -= 1
+	slime.play_hurt()
+	if health == 0:
+		queue_free()
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
 	
--- player.gd
+++ player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
 @onready var happy_boo: Node2D = %HappyBoo
-
+var health = 100.0
+@onready var hurt_box: Area2D = %HurtBox
+signal health_depleted
+@onready var progress_bar: ProgressBar = $ProgressBar
 
 func _physics_process(delta: float) -> void:
 	const SPEED = 600
@@ -13,3 +16,12 @@ func _physics_process(delta: float) -> void:
 		happy_boo.play_walk_animation()
 	else:
 		happy_boo.play_idle_animation()
+
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = hurt_box.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		progress_bar.value = health
+	if health <= 0.0:
+		health_depleted.emit()
+	
