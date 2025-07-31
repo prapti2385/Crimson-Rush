75438c4a4ea4b14d16735976ed25946c4f86813c
prapti2385 <praptikhandelwal14@gmail.com>
2025-07-31 14:54:35 +0530

    making the bullet work

--- gun.gd
+++ gun.gd
@@ -1,5 +1,4 @@
 extends Area2D
-
 func _physics_process(delta: float) -> void:
 	var enemies_in_range = get_overlapping_bodies() #returns an array
 	if enemies_in_range.size() > 0:
--- mob.gd
+++ mob.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 @onready var player: CharacterBody2D = $"../Player"
 
-
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position);
 	velocity = direction * 300.0
