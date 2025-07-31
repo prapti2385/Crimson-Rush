6da94cf4034c6686acd2cb0d75c1c40495f24e17
prapti2385 <praptikhandelwal14@gmail.com>
2025-07-31 17:20:51 +0530

    setting up shooting mechanics

--- gun.gd
+++ gun.gd
@@ -4,3 +4,7 @@ func _physics_process(delta: float) -> void:
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+
+
+func shoot():
+	
